b37f0fc494db21c4ef2e3d06c977cd7e3d29ca19
Yiannis <jbonatakis@gmail.com>
2020-06-11 22:25:39 +0200

    Update dig_test.rb
    helper Array for items examples

--- lib/libyui_client/widgets/dig_test.rb
+++ lib/libyui_client/widgets/dig_test.rb
@@ -1,35 +1,6 @@
-require "hash_dig_and_collect"
+#require "hash_dig_and_collect"
 
-client_with_many_addresses = {
-  details: {
-    first_name: "Florentino",
-    last_name: "Perez"
-  },
-  addresses: [
-    {
-      type: "home",
-      postcode: "SE1 9SG",
-      street: "London Bridge St",
-      number: 32,
-      city: "London",
-      location: {
-        latitude: 51.504382,
-        longitude: -0.086279
-      }
-    },
-    {
-      type: "office",
-      postcode: "SW1A 1AA",
-      street: "Buckingham Palace Road",
-      number: nil,
-      city: "London",
-      location: {
-        latitude: 51.5013673,
-        longitude: -0.1440787
-      }
-    }
-  ]
-}
+
 class Array
   def find_all_for(key)
     results = []
@@ -116,56 +87,3 @@ items3= [
   }
 ]
 puts items3.find_all_for(:label).to_s
-line_items = {
-  item_a: {
-    name: "Cookie",
-    total: 13
-  },
-  department_x: {
-    item_b: {
-      name: "s",
-      total: 7
-    }
-  },
-  items: [
-    {
-      name: "H",
-      total: 8
-    },
-    {
-      name: "Q",
-      total: 9
-    }
-  ]
-}
-#puts line_items.find_all_for(:total)
-items = [
-           {
-             "children": [
-               {
-                 "icon_name": "icon",
-                 "label": "node1_1"
-               },
-               {
-                 "children": [
-                   {
-                     "label": "node1_2_1"
-                   },
-                   {
-                     "label": "node1_2_2",
-                     "selected": true
-                   }
-                 ],
-                 "icon_name": "icon",
-                 "label": "node1_2"
-               }
-             ],
-             "icon_name": "icon",
-             "label": "node1",
-           },
-           {
-             "icon_name": "icon",
-             "label": "node2"
-           }
-         ]
-#puts items.map { |d| d.find_all_for(:label) }
